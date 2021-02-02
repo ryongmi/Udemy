53930d44fd0cd946c8dbff9f038a662d87948182
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-03 00:19:51 +0900

    폴더 이동

---
+++ Complete-Xamarin-Developer-Course-iOS-And-Android/TravelRecordApp/TravelRecordApp/TravelRecordApp.Android/SplashActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.IO;
+using Plugin.Permissions;
+
+namespace TravelRecordApp.Droid
+{
+    [Activity(Label = "TravelRecordApp", Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            System.Threading.Thread.Sleep(1000);
+
+            this.StartActivity(typeof(MainActivity));
+        }
+    }
+}
