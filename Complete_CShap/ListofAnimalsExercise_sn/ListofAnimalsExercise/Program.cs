40fe021bd7693bcea2a28f2160393a2bf93e818c
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-03 15:34:29 +0900

    List 챌린지
    객체를 List에 넣어 메서드 호출하기

---
+++ Complete_CShap/ListofAnimalsExercise_sn/ListofAnimalsExercise/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListofAnimalsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animals dog = new Animals();
+            dog.Name = "Gindokkae";
+            Animals cat = new Animals();
+            cat.Name = "Nabi";
+            Animals elephant = new Animals();
+            elephant.Name = "Foni";
+
+            List<Animals> list = new List<Animals>();
+            list.Add(dog);
+            list.Add(cat);
+            list.Add(elephant);
+
+            foreach (var item in list)
+            {
+                item.SayHi();
+            }
+        }
+    }
+
+    class Animals
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public void SayHi()
+        {
+            Console.WriteLine($"Hello my name is {name}");
+        }
+
+        public void Feed()
+        {
+            Console.WriteLine($"{name} is now eating");
+        }
+    }
+}
