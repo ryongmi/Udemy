72c28677f09e9b6dbbc417ef266ecea28760c687
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-03 12:10:07 +0900

    Generic sortedList
    일반 sortedList와 동일하지만,  Key와 Value의 타입을 지정할 수 있음

---
+++ Complete_CShap/GenericSortedList_sn/GenericSortedList/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericSortedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList list = new SortedList(); // non generic sortedlist
+            SortedList<string, string> phoneBook = new SortedList<string, string>(); // generic sortedlist
+
+            phoneBook.Add("Jack", "823456789");
+            phoneBook.Add("Willy", "944456789");
+            phoneBook.Add("Charles", "123456789");
+            phoneBook.Add("Jennifer", "623456789");
+            // 키 값 기준으로 자동 정렬됨
+
+            foreach (var item in phoneBook)
+            {
+                Console.WriteLine(item.Key + ", the mobile no is " + item.Value);
+            }
+
+            phoneBook["Jack"] = "123456789";
+            Console.WriteLine(phoneBook["Jack"]);
+
+            phoneBook.TryGetValue("Jack", out string number);
+            if(number != null)
+                Console.WriteLine(number);
+        }
+    }
+}
