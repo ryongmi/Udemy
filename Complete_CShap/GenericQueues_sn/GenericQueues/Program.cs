b34e442e9152ff4c70659f78ad079168ed0c8c6e
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-03 14:17:15 +0900

    Generic Queue
    큐에 제네릭을 사용하여 타입을 명시한것

---
+++ Complete_CShap/GenericQueues_sn/GenericQueues/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<DateTime> arrivals = new Queue<DateTime>();
+
+            arrivals.Enqueue(new DateTime(2017, 01, 01));
+            arrivals.Enqueue(new DateTime(2018, 12, 24));
+            arrivals.Enqueue(new DateTime(2019, 05, 25));
+
+            Console.WriteLine($"At the top {arrivals.Peek()}");
+            Console.WriteLine("------------------------");
+            foreach (var item in arrivals)
+            {
+                Console.WriteLine(item);
+            }
+
+            arrivals.Dequeue();
+            Console.WriteLine("------------------------");
+            foreach (var item in arrivals)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
