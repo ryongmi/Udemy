77e502eeacdf1b4ac7371119e581fc528ef018cd
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-03 14:07:58 +0900

    GenericStack
    Stack에 Generic을 써서 타입을 명시한것.

---
+++ Complete_CShap/GenericStacks_sn/GenericStacks/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericStacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> Ram = new Stack<string>();
+
+            Ram.Push("Visual Studio");
+            Ram.Push("Google Chrome");
+            Ram.Push("Photoshop");
+            Ram.Push("NotePad");
+
+            Console.WriteLine(Ram.Peek());
+            // 제일 위에 적재된 데이터를 반환함. ( 삭제 x )
+
+            foreach (var item in Ram)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
