40303cbf4ef23967faccf4d395c73450a2fb613d
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-03 14:46:48 +0900

    KeyValuePair
    Key와 Value를 한번에 사용할 수 있는 클래스

---
+++ Complete_CShap/KeyValuePairApp_sn/KeyValuePairApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyValuePairApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var capitals = new List<KeyValuePair<string, string>>();
+            // Key, Value를 포함하는 클래스 ( 위에 처럼 사용하면 Dictionary를 사용하는 것 같은 효과 )
+            capitals.Add(new KeyValuePair<string, string>("Russia", "Moscow"));
+            capitals.Add(new KeyValuePair<string, string>("Sweden", "Stockholm"));
+            capitals.Add(new KeyValuePair<string, string>("Portual", "Lisbon"));
+            capitals.Add(new KeyValuePair<string, string>("Japan", "Tokoyo"));
+
+            foreach (var item in capitals)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(GetFirstAndLastName());
+        }
+
+        public static KeyValuePair<string, string> GetFirstAndLastName()
+        {
+            string firstName = "Ahmad";
+            string lastName = "Mohey";
+
+            return new KeyValuePair<string, string>(firstName, lastName);
+        }
+    }
+}
