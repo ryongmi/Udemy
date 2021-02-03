eb6eb313c0b62b641c8371fd2008b41c81724f25
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-03 11:46:20 +0900

    Dictionary
    키와 값을 가지고 있고, 선언시에 타입을 지정하기 때문에 Boxing, unboxing이 일어나지 않음

---
+++ Complete_CShap/DictionaryApp_sn/DictionaryApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> capitals = new Dictionary<string, string>();
+
+            Dictionary<int, string> name = new Dictionary<int, string>()
+            {
+                { 1, "James"},
+                { 2, "Jack"},
+                { 3, "Emma"}
+                // 초기화
+            };
+            
+            foreach (var item in name)
+            {
+                Console.WriteLine(item);
+            }
+
+            capitals.Add("Russia", "Moscow");
+            capitals.Add("Italy", "Rome");
+            capitals.Add("England", "London");
+            capitals.Add("Spain", "Madrid");
+            capitals.Add("Germany", "Berlin");
+
+            //foreach (var item in capitals)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            for (int i = 0; i < capitals.Count; i++)
+            {
+                Console.WriteLine(capitals.Keys.ElementAt(i));
+
+                //Console.WriteLine(capitals.Values.ElementAt(i));
+                //Console.WriteLine(capitals[capitals.Keys.ElementAt(i)]);
+                //Console.WriteLine(capitals["Spain"]);
+                // value를 반환받을 수 있는 3가지 방법
+            }
+
+            Console.WriteLine("----------------");
+            capitals["Italy"] = "Venice";
+            Console.WriteLine($"Italy is {capitals["Italy"]}");
+
+            capitals.TryGetValue("Russia", out string result);
+            if(result != null)
+                Console.WriteLine("Result is " + result);
+
+            Console.WriteLine("----------------");
+            Console.WriteLine("After removing");
+            capitals.Remove("Italy");
+            foreach (var item in capitals)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Capital is Count : {capitals.Count}");
+
+            Console.WriteLine(capitals.ContainsKey("Italy"));
+        }
+    }
+}
