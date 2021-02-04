b003f0399a164ee828d083986a69afa03d8a43d7
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-04 11:26:55 +0900

    ListofValueTuples
    List 항목을 ValueTuple로 사용하는 방법
    List.add 및 List.Sort() 사용

---
+++ Complete_CShap/ListofValueTuples_sn/ListofValueTuples/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListofValueTuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listofPlayers = new List<(int playerNo, string playerName, int playerGoals)>();
+
+            listofPlayers.Add((7, "Ronaldo", 55));
+            listofPlayers.Add((10, "Rooney", 45));
+            listofPlayers.Add((20, "Neymar", 47));
+            listofPlayers.Add((10, "Messi", 55));
+
+            foreach (var player in listofPlayers)
+            {
+                Console.WriteLine($"{player.playerNo} - {player.playerName} - {player.playerGoals}");
+                Console.WriteLine("------------------------------------------");
+            }
+
+            listofPlayers.Sort();
+            Console.WriteLine("\nAfter Sorting");
+            foreach (var player in listofPlayers)
+            {
+                Console.WriteLine($"{player.playerNo} - {player.playerName} - {player.playerGoals}");
+                Console.WriteLine("------------------------------------------");
+            }
+        }
+    }
+}
