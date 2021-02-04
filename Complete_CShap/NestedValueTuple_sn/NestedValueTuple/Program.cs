e4a3edaa2ce8663d48e48c8766470196f6882afc
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-04 10:58:38 +0900

    NestedValueTuple
    ValueTuple에서 이중 튜플을 사용하는 법
    ( Tuple과 똑같음 )

---
+++ Complete_CShap/NestedValueTuple_sn/NestedValueTuple/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedValueTuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ValueTuple에서 이중 Tuple 사용
+            var ronaldoGoals = (Year2017:64, Year2016:60, Year2015:50, Year2014: 55);
+            var ronaldo = (playerNo: 7, playerName: "Ronaldo", playerGoals: ronaldoGoals);
+
+            Console.WriteLine(ronaldo.playerNo);
+            Console.WriteLine(ronaldo.playerName);
+            Console.WriteLine(ronaldo.playerGoals.Year2015);
+            Console.WriteLine(ronaldo.playerGoals.Year2016);
+            Console.WriteLine(ronaldo.playerGoals.Year2017);
+        }
+    }
+}
+
