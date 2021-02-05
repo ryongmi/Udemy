b8485b4769685c91c3a835d1b5bb2cb3c6eb3fee
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-05 22:26:20 +0900

    챌린지 13
    튜플 안에 튜플 넣기

---
+++ Complete_CShap/Assignments13_sn/Assignments13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var empInfo = new Tuple<string, string, string, string, string, string, string,
+                              Tuple<decimal, decimal, decimal, decimal, decimal, decimal, decimal,
+                              Tuple<int, int, int, int, int>>>("", "", "", "", "", "", "",
+                                new Tuple<decimal, decimal, decimal, decimal, decimal, decimal, decimal, Tuple<int, int, int, int, int>>
+                                         (0m, 1m, 2m, 3m, 4m, 5m, 6m, Tuple.Create(100, 200, 300, 400, 500)));
+
+            Console.WriteLine(empInfo.Rest.Rest.Item5);
+        }
+    }
+}
