a33a91d0bb86ebae68f00f608cafa187c4cce4c2
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-06 17:52:58 +0900

    제네릭 인수
    제네릭 함수를 정의하여 제네릭 매개변수로 서로 다른 타입 값으로 함수 호출 하는 방법

---
+++ Complete_CShap/GenericArguments_sn/GenericArguments/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericArguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayInfo<string>("Generic");
+            DisplayInfo<int>(1);
+            DisplayInfo<decimal>(1.5m);
+            // Generic 함수 호출
+            
+            DisplayInfo(1);
+            // <int>를 명시하지 않고도 사용가능
+
+            var playerInfo = (7, "Ronaldo", 55);
+            DisplayInfo(playerInfo);
+        }
+
+        public static void DisplayInfo<T>(T info)
+        // Generic 함수 정의
+        {
+            Console.WriteLine(info);
+        }
+    }
+}
