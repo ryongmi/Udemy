61f0afe883a60008d03fab5083e69a11497300ef
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-08 16:35:37 +0900

    DockPanel, Canvas, ComboBox, Properties_Settings
    DockPanel, Canvas 배치 및 Background  color 넣기, ComboBox Item 추가 및 이벤트 추가, Properties의 설정에서 변수 설정이 가능함. 이 변수는 로직에서 사용 가능하며, 프로그램에서 값이 변하면 저장도 가능 ( 다음 실행에서 변경된 값으로 시작할 수 있음 )

---
+++ WPF-in-C#-for-Beginners-Windows-Presentation-Foundation-XAML/wpf_diary_app_sn/wpf_diary_app/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_diary_app
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pic_cbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        // 콤보박스 아이템 변경되면 발생하는 이벤트
+        {
+            String path;
+            if(pic_cbox.SelectedIndex == 0)
+                path = Environment.CurrentDirectory + "\\Data\\Pics\\" + (pic_cbox.SelectedIndex + 1).ToString() + ".png";
+            else
+                path = Environment.CurrentDirectory + "\\Data\\Pics\\" + (pic_cbox.SelectedIndex + 1).ToString() + ".jpg";
+            System.Uri ur = new Uri(path);
+            // 이미지 경로 받아옴
+            ImageBrush im = new ImageBrush(new BitmapImage(ur));
+            // 이미지 uri를 이미지 브러쉬 객체 생성 -> window background 이미지가 imagebrush를 사용해서 이미지브러쉬 객체 im을 만들어야함
+            im.Stretch = Stretch.Fill;
+
+            this.Background = im;
+            // 백그라운드 이미지 변경
+
+            wpf_diary_app.Properties.Settings.Default.sel_image_main_window = pic_cbox.SelectedIndex;
+            // 콤보박스 인덱스가 변경될때마다 설정 값도 바꿔줌
+
+            wpf_diary_app.Properties.Settings.Default.Save();
+            // 바꾼 설정 값을 저장함. ( 다음에 실행할때 마지막까지 사용했던 값을 계속 사용할 수 있음 )
+        }
+
+        private void main_window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Int32 fi;
+            fi = wpf_diary_app.Properties.Settings.Default.sel_image_main_window;
+            // Properties 설정에서 설정한 값을 사용하는 구문
+
+            String path;
+            if (fi == 0)
+                path = Environment.CurrentDirectory + "\\Data\\Pics\\" + (fi + 1).ToString() + ".png";
+            else
+                path = Environment.CurrentDirectory + "\\Data\\Pics\\" + (wpf_diary_app.Properties.Settings.Default.sel_image_main_window + 1).ToString() + ".jpg";
+            System.Uri ur = new Uri(path);
+            
+            ImageBrush im = new ImageBrush(new BitmapImage(ur));
+            
+            im.Stretch = Stretch.Fill;
+
+            this.Background = im;
+
+            pic_cbox.SelectedIndex = fi;
+            // 백그라운드 이미지를 설정하면서 콤보박스 인덱스도 변경해 줌
+        }
+    }
+}
