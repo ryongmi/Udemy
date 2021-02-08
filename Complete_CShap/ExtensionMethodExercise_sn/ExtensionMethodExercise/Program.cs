967fb73dd43cda27c9949255ae33de05d9695926
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-08 21:03:31 +0900

    this 매개변수
    this 매개변수는 객체가 메서드 호출하는 것처럼 호출가능함.
    호출시 변수가 매개변수 값이 되며, 따로 객체를 생성하거나 클래스 참조를 해줄 필요가 없다.

---
+++ Complete_CShap/ExtensionMethodExercise_sn/ExtensionMethodExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myDate = new DateTime(2000, 11, 1);
+            Console.WriteLine(myDate.MyCustomFormat());
+        }
+    }
+
+    static class MyExtensions
+    {
+        public static string MyCustomFormat(this DateTime date)
+        // 매개변수 this는 함수 호출시 객체에서 메서드 호출하는 것처럼 "변수.함수"로 호출이 가능함. 여기서 변수가 매개변수 date가 된다.
+        {
+            return string.Format("{0:dddd dd \nMMMM yyyy}", date);
+        }
+    }
+}
