b9424d3f2eadaeab2ebea7953cfa80952d774e50
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-10 15:36:56 +0900

    LambdaExpression
    간략한 람다 사용

---
+++ Complete_CShap/LambdaExpression_sn/LambdaExpression/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{
+    delegate void DisplayMessage();
+    delegate int Multiply(int n);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // => 뒤의 오는 문장을 반환함
+
+            Multiply MultiplyNumber = n => n * 3;
+            // n*3을 반환
+            Console.WriteLine(MultiplyNumber(30));
+
+            DisplayMessage Message = () => Console.WriteLine("Hi from the lambda expression");
+            // 출력문을 반환
+            Message();
+        }
+    }
+}
