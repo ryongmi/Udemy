dfdf2145013f621deeb5c0ae50d3288b4dc45082
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-10 22:47:46 +0900

    Action Delegate lambda
    Action 메서드 람다 사용

---
+++ Complete_CShap/ActionWithAnonymousAndLambda_sn/ActionWithAnonymousAndLambda/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionWithAnonymousAndLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jack";
+
+            Action<string> Message = ShowMessage;
+            Message(name);
+
+            name = "Dog";
+            Action<string> anonymousMethod = delegate (string myName) { Console.WriteLine($"Hi, how are you today, {myName}?"); };
+            anonymousMethod(name);
+
+            name = "Cat";
+            Action<string> labmdaExpression = myName => Console.WriteLine($"Hi, how are you today, {myName}?");
+            labmdaExpression(name);
+        }
+
+        public static void ShowMessage(string name)
+        {
+            Console.WriteLine($"Hi, how are you today, {name}?");
+        }
+    }
+}
