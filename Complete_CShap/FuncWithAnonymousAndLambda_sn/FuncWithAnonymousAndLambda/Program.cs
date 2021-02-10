a80118f304f6002f233e95f13e54582a344f88e4
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-10 22:40:59 +0900

    Func Delegate Lambda
    Func에 람다식 사용

---
+++ Complete_CShap/FuncWithAnonymousAndLambda_sn/FuncWithAnonymousAndLambda/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncWithAnonymousAndLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Ahmad Mohey";
+
+            Func<string, string> ConvertToUppercase = UppercaseString;
+            Console.WriteLine("using func ");
+            Console.WriteLine(ConvertToUppercase(name));
+
+            Func<string, string> anonymousMethod = delegate (string myName) { return myName.ToUpper(); };
+            Console.WriteLine("using anonymous method ");
+            Console.WriteLine(anonymousMethod(name));
+
+            Func<string, string> lambdaExpression = text => text.ToUpper();
+            Console.WriteLine("using lambda expression ");
+            Console.WriteLine(lambdaExpression(name));
+        }
+
+        public static string UppercaseString(string inputString)
+        {
+            return inputString.ToUpper();
+        }
+    }
+}
