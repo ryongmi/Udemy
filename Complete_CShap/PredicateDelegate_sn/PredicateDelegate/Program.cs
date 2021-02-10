b9f96757ead2fb97c319dbbd1bbd44c127d41a68
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-10 22:28:57 +0900

    Predicate Delegate
    bool 반환 받는 델리게이트 메서드

---
+++ Complete_CShap/PredicateDelegate_sn/PredicateDelegate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PredicateDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> condition = IsAdmin;
+            Console.WriteLine(condition(11));
+        }
+
+        public static bool IsAdmin(int empNo)
+        {
+            if (empNo == 10)
+                return true;
+            else
+                return false;
+        }
+    }
+}
