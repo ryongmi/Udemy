65b535bab3955fb9ae60cac248144adc12cbdfac
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-10 23:04:31 +0900

    Event
    Delegate의 이벤트를 만들어 생성자를 이용해 객체가 생성될 때마다 메서드를 이벤트에 추가하여 사용

---
+++ Complete_CShap/Events_sn/Events/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public delegate void myDelegate(string name);
+
+    class Program
+    {
+        public event myDelegate myEvent;
+        
+        public Program()
+        {
+            // 객체가 만들어지면 3함수의 이벤트 생성
+            myEvent += new myDelegate(SayHiEnglish);
+            myEvent += new myDelegate(SayHiArabic);
+            myEvent += new myDelegate(SayHiItalian);
+        }
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            prog.myEvent("Jack");
+            // 이벤트 호출시 객체 생성하면서 걸려있던 함수들을 호출
+        }
+
+        public void SayHiEnglish(string name)
+        {
+            Console.WriteLine("Hi " + name);
+        }
+
+        public void SayHiArabic(string name)
+        {
+            Console.WriteLine("Marhaaban " + name);
+        }
+
+        public void SayHiItalian(string name)
+        {
+            Console.WriteLine("Caio " + name);
+        }
+    }
+}
