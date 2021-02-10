ed70c675a9a411733e2d4a3f91d4cd99986bb0ef
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-10 15:57:32 +0900

    Func Delegate
    Func는 Delegate와 같이 메서드를 참조하여 사용하며, Generic으로 명시한 데이터 타입 개수 -1 개 만큼 매개변수를 사용, -1개는 반환 받을 데이터 타입을 정의한것

---
+++ Complete_CShap/FuncDelegate_sn/FuncDelegate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> funcOne = AddTwoNumbers;
+            // 함수 델리게이트 : Generic <>으로 정의한 개수만큼(마지막 1개는 제외, 반환 데이터 타입) 매개변수를 사용, 델리게이트와 똑같이 메서드 참조하여 사용
+            // int 타입의 반환값을 받음.
+            Console.WriteLine(AddTwoNumbers(3, 10));
+
+            Func<int> funcTwo = AddTwoNumbers;
+            Console.WriteLine(funcTwo());
+        }
+
+        public static int AddTwoNumbers(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int AddTwoNumbers()
+        {
+            int z = 0;
+            int x = 5;
+            int y = 2;
+
+            z = x + y;
+
+            return z;
+        }
+    }
+}
