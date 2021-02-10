926eb08666143deccd0f14c6e699f4bb8445d280
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-10 13:13:21 +0900

    Multicast Delegate
    델리게이트에 메서드를 여러개 참조시켜 델리게이트를 호출할때 여러 메서드를 한번에 호출하는 효과

---
+++ Complete_CShap/MulticastDelegate_sn/MulticastDelegate/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulticastDelegate
+{
+    public delegate void SayHiDelegate();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHiDelegate sayHi = null;
+            // SayHiDelegate sayHi; 과 다른점?
+
+            sayHi = new SayHiDelegate(SayHiEnglish);
+            sayHi += new SayHiDelegate(SayHiSpanish);
+            sayHi += new SayHiDelegate(SayHiJapanese);
+            sayHi += new SayHiDelegate(SayHiItalian);
+            sayHi += new SayHiDelegate(SayHiGerman);
+            sayHi += new SayHiDelegate(SayHiArabic);
+            // 멀티 델리게이트 사용
+
+            sayHi();
+        }
+
+        public static void SayHiEnglish()
+        {
+            Console.WriteLine("Hi there.");
+        }
+
+        public static void SayHiSpanish()
+        {
+            Console.WriteLine("Hola.");
+        }
+
+        public static void SayHiJapanese()
+        {
+            Console.WriteLine("Kon'nichiwa.");
+        }
+
+        public static void SayHiItalian()
+        {
+            Console.WriteLine("Ciao.");
+        }
+
+        public static void SayHiGerman()
+        {
+            Console.WriteLine("Hallo.");
+        }
+
+        public static void SayHiArabic()
+        {
+            Console.WriteLine("Marhabaan.");
+        }
+    }
+}
