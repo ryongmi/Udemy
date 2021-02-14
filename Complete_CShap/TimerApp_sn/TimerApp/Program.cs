7cd19ad691cecc0cf431f75c06e25f1c4d5b69e0
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-14 19:46:51 +0900

    Timer
    타이머 시간 설정, 메서드 추가, 타이머 시작 메서드

---
+++ Complete_CShap/TimerApp_sn/TimerApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TimerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer();
+            timer.Elapsed += Timer_Elapsed;
+            // 정해진 시간마다 발생, 추가한 메서드를 호출한다.
+            timer.Interval = 1000;
+            // 시간 설정, 1000 = 1초
+            //timer.Enabled = true;
+            // 타이머 시작, 같은 명령어 : timer.Start();
+
+            Console.WriteLine("Press (x) to exit");
+            while (Console.Read() != 'x') ;
+        }
+
+        private static void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Yes it is working " + DateTime.Now.ToLongTimeString());
+        }
+    }
+}
