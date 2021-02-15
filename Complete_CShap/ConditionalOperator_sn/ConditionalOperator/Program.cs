a479edff35c15fd8827c13cce1e15e875aeb1192
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-15 18:21:06 +0900

    Conditional Operator
    조건부 연산자
    평가값 ? Ture일 때 실행 부분 : false일 때 실행 부분

---
+++ Complete_CShap/ConditionalOperator_sn/ConditionalOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsAdmin() ? "Yes an admin" : "No not an admin");
+            Console.WriteLine(!IsAdmin() ? "Yes an admin" : "No not an admin");
+            // 조건부 연산자 : bool 변수 ? True일 때 출력값(실행 부분) : false일 때 출력값(실행 부분)
+        }
+
+        public static bool IsAdmin()
+        {
+            return false;
+        }
+    }
+}
