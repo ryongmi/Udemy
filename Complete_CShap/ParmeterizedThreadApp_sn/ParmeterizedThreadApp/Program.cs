ea391d67b36bc3e1f3fc2bc19f4cbbf28c391025
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-24 16:23:00 +0900

    Thread ParameterizedThreadStart
    ParameterizedThreadStart
    -> 쓰레드가 실행될 때 매개변수로 값을 보낼수 있게 해줌

---
+++ Complete_CShap/ParmeterizedThreadApp_sn/ParmeterizedThreadApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParmeterizedThreadApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Helper helper = new Helper();
+            Thread t1 = new Thread(new ParameterizedThreadStart(helper.Loop));
+            // ParameterizedThreadStart -> 쓰레드가 실행될 때 매개변수로 값을 받아 갈 수 있게 해줌
+            t1.Start(500);
+        }
+    }
+
+    class Helper
+    {
+        public void Loop(object number)
+        {
+            for (int i = 0; i < int.Parse(number.ToString()); i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
