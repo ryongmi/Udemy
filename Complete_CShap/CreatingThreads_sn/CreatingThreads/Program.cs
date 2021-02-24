0409f9229e2752a280cb2157a602c311ed89cd62
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-02-24 15:39:51 +0900

    Thread
    Sleep() 메서드 : 쓰레드를 정해진 시간동안 정지시킴
    Abort() 메서드 : 예외를 반한하고 쓰레드를 종료시킴
    Join() 메서드 : 호출자 쓰레드를 실행시키고 호출자가 종료되기 전까지 다른 쓰레드를 정지시킨다.

--- Complete_CShap/CreatingThreads_sn/CreatingThreads/Program.cs
+++ Complete_CShap/CreatingThreads_sn/CreatingThreads/Program.cs
@@ -9,16 +9,19 @@ namespace CreatingThreads
 {
     class Program
     {
+        static Thread t1;
+        static Thread t2;
+
         static void Main(string[] args)
         {
-            Thread t1 = new Thread(new ThreadStart(SayHiEnglish));
+            t1 = new Thread(new ThreadStart(SayHiEnglish));
             // ThreadStart(메서드 A) -> 스레드가 실행되면 실행할 메서드를 설정(메서드 A로 설정함)
             t1.Name = "Thread Number 1 (English)";
             // 스레드 객체 t1의 이름 설정
             t1.Start();
             // t1 스레드 실행
 
-            Thread t2 = new Thread(new ThreadStart(SayHiSpanish));
+            t2 = new Thread(new ThreadStart(SayHiSpanish));
             t2.Name = "Thread Number 2 (Spanish)";
             t2.Start();
         }
@@ -31,7 +34,23 @@ namespace CreatingThreads
 
             for (int i = 0; i < 50; i++)
             {
+                /*
+                Thread.Sleep(1000);
+                // Sleep(Time) -> 스레드를 정해진 시간동안 멈춰놓음. Time : 멈춰 놓을 시간, 1000 = 1초 
+                */
+
+                /*
+                if (i == 31)
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + " is about to be aborted");
+                    t1.Abort();
+                    // Abort() : 스레드를 종료하는 메서드, 실행하면 예외를 반환하고 스레드가 종료된다.
+                }
+                */
+
                 Console.WriteLine(i + "Hi...");
+                t2.Join();
+                // Join() -> t2 스레드를 실행하고, t2가 끝날때까지 t1은 대기상태에 놓임
             }
         }
 
@@ -41,6 +60,8 @@ namespace CreatingThreads
 
             for (int i = 0; i < 50; i++)
             {
+                Thread.Sleep(new TimeSpan(0, 0, 1));
+                // Sleep 메서드의 시간을 설정할 수 있는 다른 방법
                 Console.WriteLine(i + "Hola...");
             }
         }
