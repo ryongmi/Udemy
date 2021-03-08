9499fd86101e7032aaa73a5b9af5c8dd49d1de0e
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-03-08 15:50:24 +0900

    Task<Generic>
    Task와 마찬가지로 비동기 작업을 수행하지만, 반환 값이 있다.

---
+++ Complete_CShap/TasksWithReturnValue_sn/TasksWithReturnValue/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksWithReturnValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> task = new Task<int>(() =>
+            // Task<int> : Task는 반환값이 없는 비동기 작업을 수행하지만, 제네릭을 붙여주면, 지정한 타입의 반환 값이 있는 비동기 작업을 수행한다. ( 제네릭을 붙이면 반환값이 있음 )
+            {
+                int result = 0;
+                for (int i = 0; i <= 5; i++)
+                {
+                    result += i;
+                    Console.WriteLine(i);
+                }
+                return result;
+            });
+            task.Start();
+            Console.WriteLine(task.Result);
+
+            Console.ReadLine();
+        }
+    }
+}
