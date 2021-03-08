41b70a25793511785b942463b2acb8d5f656070c
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-03-08 16:59:11 +0900

    Task 모듈, Run 메서드
    Run 메서드로 Task 실행, Task 모듈화

---
+++ Complete_CShap/TaskRunApp_sn/TaskRunApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskRunApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHithod();
+
+            AddNumbersTask(10, 12, 13, 45, 11);
+
+            Console.ReadLine();
+        }
+
+        private static Task SayHithod()
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine("Hi there from the task");
+            });
+        }
+        public static Task AddNumbersTask(params int[] numbers)
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine("Add numbers task started...");
+                int result = 0;
+                foreach (var number in numbers)
+                {
+                    result += number;
+                }
+                Console.WriteLine("Total = " + result);
+            });
+        }
+    }
+}
