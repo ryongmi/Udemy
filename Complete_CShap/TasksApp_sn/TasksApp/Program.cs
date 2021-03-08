b10ac576a7339b3e1208b3fd6cf4b4397e53c262
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-03-08 15:34:46 +0900

    Task
    매개변수가 없는 메서드 지정하여 사용

---
+++ Complete_CShap/TasksApp_sn/TasksApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TasksApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task = new Task(new Action(SayHi));
+            task.Start();
+
+            Task task2 = new Task(delegate ()
+            {
+                Console.WriteLine("Task 2 is starting");
+                Console.WriteLine("Task 2 is running");
+                Console.WriteLine("Hi there from task 2");
+                Thread.Sleep(1500);
+                Console.WriteLine("Task 2 is complete");
+            });
+            task2.Start();
+
+            Task task3 = new Task(() =>
+            {
+                Console.WriteLine("Task 3 is starting");
+                Console.WriteLine("Task 3 is running");
+                Console.WriteLine("Hi there from task 3");
+                Thread.Sleep(1500);
+                Console.WriteLine("Task 3 is complete");
+            });
+            task3.Start();
+
+            Console.WriteLine("What is your name");
+            var name = Console.ReadLine();
+            Console.WriteLine("Welcome back " + name);
+
+            Console.ReadLine();
+        }
+
+        public static void SayHi()
+        {
+            Console.WriteLine("Task 1 is starting");
+            Console.WriteLine("Task 1 is running");
+            Console.WriteLine("Hi there from task 1");
+            Thread.Sleep(1500);
+            Console.WriteLine("Task 1 is complete");
+        }
+    }
+}
