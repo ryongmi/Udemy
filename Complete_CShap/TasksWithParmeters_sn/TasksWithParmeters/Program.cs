7f931d55db4f5276944a85af3b68f389b515657d
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-03-08 15:43:53 +0900

    Task
    매개변수가 있는 Task 생성

---
+++ Complete_CShap/TasksWithParmeters_sn/TasksWithParmeters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksWithParmeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task1 = new Task(new Action<object>(SayHi), "Jennifer");
+            task1.Start();
+
+            Task task2 = new Task(delegate (object name)
+            {
+                Console.WriteLine("Welcome back " + name);
+            }, "Ahmad");
+            task2.Start();
+
+            Task task3 = new Task(name => { Console.WriteLine("Welcome back " + name); }, "Jack");
+            task3.Start();
+
+            Console.ReadLine();
+        }
+
+        public static void SayHi(object name)
+        {
+            Console.WriteLine("Welcome back " + name);
+        }
+    }
+}
