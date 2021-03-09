79a17a853872bafd7c3ae5d681d81460d5a5c437
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-03-09 16:30:35 +0900

    SpeechSynthesizer
    텍스트를 음성을 스피커로 출력 시킬 수 있는 클래스
    저장도 가능하며, 비동기로 음성, 문자를 출력 가능

---
+++ Complete_CShap/SpeechSynthesizerApp_sn/SpeechSynthesizerApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace SpeechSynthesizerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpeechSynthesizer robot = new SpeechSynthesizer();
+
+            robot.SelectVoiceByHints(VoiceGender.Female);
+            // 음성의 성별을 정함
+            robot.Volume = 45;
+            robot.Rate = 0;
+            // Rate :  말하기 속도, -10 ~ 10 사이의 값
+
+            // robot.Speak("Hello, What is your name?");
+            robot.SpeakAsync("Hello, What is your name?");
+            // SpeakAsync : 비동기 말하기, 음성이 나오면서 프로그램이 진행됨
+            Console.WriteLine("Hello, What is your name?");
+            var name = Console.ReadLine();
+            robot.SpeakAsync("Hello " + name);
+            Console.WriteLine("Hello " + name);
+
+            Console.ReadLine();
+
+            robot.SetOutputToWaveFile(@"c:\sounds\app.wav");
+            // SetOutputToWaveFile : 음성파일 저장, 아래에 나온 speak(x) 안에 있는 x를 app.wav로 저장한다. ( 실제 x 음성이 들림 )
+            robot.Speak("Application is complete, please visit us again");
+        }
+    }
+}
