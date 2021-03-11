4e44fc76526cc7cdd86ac1ddf95e68f3b7d9f98d
ryongmi <62281754+ryongmi@users.noreply.github.com>
2021-03-11 10:32:06 +0900

    Process 클래스
    로컬에서 프로세스 제어

---
+++ Complete_CShap/StartingAndStoppingProcess_sn/StartingAndStoppingProcess/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace StartingAndStoppingProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("Notepad.exe");
+            // Process 클래스, Start() 괄호안에 있는 프로세스를 실행한다.
+
+            Process[] notepads = Process.GetProcessesByName("notepad");
+            foreach (var process in notepads)
+            {
+                process.Kill();
+            }
+
+            Process[] GetProcess = Process.GetProcesses();
+            // GetProcesses() : 현재 로컬에서 실행중인 프로세스 목록 반환
+            
+            foreach (var process in GetProcess)
+            {
+                Console.WriteLine(process.Id + " / " + process.ProcessName + " / " + process.MainWindowTitle);
+                
+            }
+        }
+    }
+}
