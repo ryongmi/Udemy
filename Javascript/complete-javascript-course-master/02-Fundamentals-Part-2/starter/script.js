ccbf473c63725d401446084896dce5836373117d
ryongmi <62281754+ryongmi@users.noreply.github.com>
2022-10-08 23:47:20 +0900

    자바스크립트 반복문, 객체 사용
    자바스크립트 반복문, 객체 사용

--- Javascript/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ Javascript/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -21,32 +21,32 @@ HINT: To check if number A is at least double number B, check for A >= 2 * B. Ap
 GOOD LUCK 😀
 */
 
-console.log('Coding Challenge #1');
+// console.log('Coding Challenge #1');
 
-// calcAverage -> 함수를 담고 있는 변수가 됨. 
-const calcAverage = (Data1, Data2, Data3) => (Data1 + Data2 + Data3) / 3;
+// // calcAverage -> 함수를 담고 있는 변수가 됨. 
+// const calcAverage = (Data1, Data2, Data3) => (Data1 + Data2 + Data3) / 3;
 
-// calcAverage를 호출하여 평균값을 계산
-const avgDolhins1 = calcAverage(44, 23, 71);
-const avgKoalas1 = calcAverage(65, 54, 49);
+// // calcAverage를 호출하여 평균값을 계산
+// const avgDolhins1 = calcAverage(44, 23, 71);
+// const avgKoalas1 = calcAverage(65, 54, 49);
 
-const avgDolhins2 = calcAverage(85, 54, 41);
-const avgKoalas2 = calcAverage(23, 34, 27);
+// const avgDolhins2 = calcAverage(85, 54, 41);
+// const avgKoalas2 = calcAverage(23, 34, 27);
 
-console.log(avgDolhins1, avgKoalas1, avgDolhins2, avgKoalas2);
+// console.log(avgDolhins1, avgKoalas1, avgDolhins2, avgKoalas2);
 
-function checkWinner(avgDolhin, avgKoala) {
-    if (avgDolhin >= 2 * avgKoala) {
-        console.log(`Dolphins win 🏆 (${avgDolhin} vs. ${avgKoala})`);
-    } else if (avgKoala >= 2 * avgDolhin) {
-        console.log(`Koalas win 🏆 (${avgKoala} vs. ${avgDolhin})`);
-    } else {
-        console.log('No team wins...');
-    }
-}
+// function checkWinner(avgDolhin, avgKoala) {
+//     if (avgDolhin >= 2 * avgKoala) {
+//         console.log(`Dolphins win 🏆 (${avgDolhin} vs. ${avgKoala})`);
+//     } else if (avgKoala >= 2 * avgDolhin) {
+//         console.log(`Koalas win 🏆 (${avgKoala} vs. ${avgDolhin})`);
+//     } else {
+//         console.log('No team wins...');
+//     }
+// }
 
-checkWinner(avgDolhins1, avgKoalas1);
-checkWinner(avgDolhins2, avgKoalas2);
+// checkWinner(avgDolhins1, avgKoalas1);
+// checkWinner(avgDolhins2, avgKoalas2);
 
 
 ///////////////////////////////////////
@@ -67,7 +67,96 @@ HINT: Remember that an array needs a value in each position, and that value can 
 GOOD LUCK 😀
 */
 
-console.log('Coding Challenge #2');
+// console.log('Coding Challenge #2');
+
+// function calcTip(bill) {
+//     if (50 <= bill <= 300)
+//         return bill * 0.15;
+//     else
+//         return bill * 0.2;
+// }
+
+// // 또 다른 배열 생성방법
+// //const bills = [125, 555, 44];
+// const bills = new Array(125, 555, 44);
+// const tip = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const total = new Array(bills[0] + tip[0], bills[1] + tip[1], bills[2] + tip[2]);
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(total[i]);
+// }
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/*
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+
+1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method.
+3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+
+GOOD LUCK 😀
+*/
+
+console.log('Coding Challenge #3');
+
+const Mark = {
+    fullName: "Mark Miller",
+    mass: 78,
+    height: 1.69,
+    bmi: 0,
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+};
+
+const John = {
+    fullName: "John Smith",
+    mass: 92,
+    height: 1.95,
+    bmi: 0,
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+};
+
+if (Mark.calcBMI() > John.calcBMI())
+    console.log(`${Mark.fullName} BMI (${Mark.bmi}) is higher than ${John.fullName} (${John.bmi})!`);
+else if (Mark.calcBMI() < John.calcBMI())
+    console.log(`${John.fullName} BMI (${John.bmi}) is higher than ${Mark.fullName} (${Mark.bmi})!`);
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Let's improve Steven's tip calculator even more, this time using loops!
+
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+
+TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+
+HINT: Call calcTip in the loop and use the push method to add values to the tips and totals arrays 😉
+
+4. BONUS: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it:
+  4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
+  4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
+  4.3. Call the function with the 'totals' array
+
+GOOD LUCK 😀
+*/
+
+console.log('Coding Challenge #4');
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
 
 function calcTip(bill) {
     if (50 <= bill <= 300)
@@ -76,12 +165,20 @@ function calcTip(bill) {
         return bill * 0.2;
 }
 
-// 또 다른 배열 생성방법
-//const bills = [125, 555, 44];
-const bills = new Array(125, 555, 44);
-const tip = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-const total = new Array(bills[0] + tip[0], bills[1] + tip[1], bills[2] + tip[2]);
-
-for (let i = 0; i < 3; i++) {
-    console.log(total[i]);
+for (let index = 0; index < bills.length; index++) {
+    let tip = calcTip(bills[index]);
+    tips.push(tip);
+    totals.push(bills[index] + tip);
 }
+
+console.log(bills, tips, totals);
+
+function calcAverage(arr) {
+    let sum = 0;
+    for (let index = 0; index < arr.length; index++) {
+        sum += arr[index]
+    }
+    return sum / arr.length;
+}
+
+console.log(calcAverage(totals));
