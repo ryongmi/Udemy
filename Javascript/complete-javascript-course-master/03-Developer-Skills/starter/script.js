12db7f0631fc8e00980e918eb215f480037cf773
ryongmi <62281754+ryongmi@users.noreply.github.com>
2022-10-10 18:18:17 +0900

    챕터 4 - 챌린지
    배열 사용

--- Javascript/complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ Javascript/complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,4 +1,34 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
-const x = 23;
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+console.log("Coding Challenge #1");
+
+function printForecast(arr) {
+  let msg = "... ";
+  for (let index = 0; index < arr.length; index++) {
+    msg += `${arr[index]} ºC in ${index} days ... `;
+  }
+  console.log(msg);
+}
+
+const Data1 = [17, 21, 23];
+const Data2 = [12, 5, -5, 0, 4];
+
+printForecast(Data1);
+printForecast(Data2);
