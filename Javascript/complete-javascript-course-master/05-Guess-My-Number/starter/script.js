812432c4d9eb5581652bd96faa048fc55bf45eb9
ryongmi <62281754+ryongmi@users.noreply.github.com>
2022-10-11 13:39:20 +0900

    프로젝트 1 - 번호추측게임
    랜덤함수를 이용해 1~20 중 임의의 숫자를 선택해 그 숫자를 맞추는 게임

--- Javascript/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ Javascript/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = '🎉 Correct Number!';
@@ -9,4 +10,72 @@ document.querySelector('.score').textContent = 10;
 
 // 입력하는 컨트롤일때는 value를 사용해야함
 document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+console.log(document.querySelector('.guess').value); 
+*/
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+function funScoreMinus(nowScore) {
+  score = nowScore;
+  document.querySelector('.score').textContent = score;
+}
+
+function funChangeMsg(msg) {
+  document.querySelector('.message').textContent = msg;
+}
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    funChangeMsg('⛔ No Number!');
+  } else if (guess === secretNumber) {
+    funChangeMsg('🎉 Correct Number!');
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      funChangeMsg(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      funScoreMinus(--score);
+    } else {
+      funChangeMsg('💥 You lost the game!');
+      funScoreMinus(0);
+    }
+  }
+});
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+});
