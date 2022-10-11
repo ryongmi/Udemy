e7ab5f690119dad4e6c4f011827268f36f719131
ryongmi <62281754+ryongmi@users.noreply.github.com>
2022-10-11 10:20:20 +0900

    Dom 조작
    입력이 필요한 컨트롤 : value
    입력이 필요없는 컨트롤 : txtContent

--- Javascript/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ Javascript/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+// 입력하는 컨트롤일때는 value를 사용해야함
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
