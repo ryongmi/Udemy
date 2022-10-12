1c318e28390fb97659eb89dbcf1cd71b9d1dbe14
ryongmi <62281754+ryongmi@users.noreply.github.com>
2022-10-12 16:24:05 +0900

    주사위 게임
    toggle : () 안에 있는 값을 가지고 있다면 삭제, 없다면 생성해주는 메서드

--- Javascript/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ Javascript/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,1 +1,44 @@
 'use strict';
+
+const player0El = document.querySelector(`.player--0`);
+const player1El = document.querySelector(`.player--1`);
+
+// querySelector : Id - #, class - .
+const score0El = document.querySelector('#score--0');
+
+// Id를 찾아 가져오는 메서드, 앞에 붙이는게 없음
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+let activePlayer = 0;
+
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6 + 1);
+
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+});
