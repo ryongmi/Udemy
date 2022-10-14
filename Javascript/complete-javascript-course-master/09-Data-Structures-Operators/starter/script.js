81f7de3a8849fea212e7e5b708e0cc35dc87c378
ryongmi <62281754+ryongmi@users.noreply.github.com>
2022-10-14 16:26:58 +0900

    배열 값 변수에 할당하는 방법

--- Javascript/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ Javascript/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -27,3 +27,20 @@ const restaurant = {
     },
   },
 };
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// 변수 선언하면서 배열의 값을 할당받는 방법
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+// [] 안에 있는 순서대로 restaurant.categories[0], restaurant.categories[1], restaurant.categories[2]의 값을 할당받음
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// 변수의 값을 교환하는 방법
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
