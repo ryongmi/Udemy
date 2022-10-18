ab8bcec8b1f6fc53862d6efb2136b04683c900c2
ryongmi <62281754+ryongmi@users.noreply.github.com>
2022-10-18 17:22:35 +0900

    Challenge 1
    객체 메서드에 this를 사용할 때, html 컨트롤 이벤트에 객체 메서드를 사용
    타입비교, 사용자 입력값 받아서 개체 속성 할당

--- Javascript/complete-javascript-course-master/10-Functions/starter/script.js
+++ Javascript/complete-javascript-course-master/10-Functions/starter/script.js
@@ -139,3 +139,36 @@ BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 
 GOOD LUCK 😀
 */
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const str = `${this.question}\n${this.options.join(
+      '\n'
+    )}\n(Write option number)`;
+
+    const option = Number(prompt(str));
+    if (typeof option !== 'number') return;
+    if (0 > option || option > 3) return;
+
+    this.answers[option]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'arr') {
+    if (type === 'arr') console.log(this.answers);
+    else if (type === 'string')
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
