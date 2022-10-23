a757011c365ea6825ac73854c84af4ef6b348f33
ryongmi <62281754+ryongmi@users.noreply.github.com>
2022-10-24 00:28:59 +0900

    입출금, 이자 계산
    입출금, 이자 계산

--- Javascript/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ Javascript/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -87,6 +87,47 @@ function displayMovements(movements) {
 
 displayMovements(account1.movements);
 
+function calcDisplayBalance(movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} €`;
+}
+calcDisplayBalance(account1.movements);
+
+function calcDisplaySummary(movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes} €`;
+
+  const outcomes = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(outcomes)} €`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => int >= 1)
+    .reduce((acc, int) => acc + int, 0);
+
+  labelSumInterest.textContent = `${interest} €`;
+}
+
+calcDisplaySummary(account1.movements);
+
+function createUsernames(accs) {
+  accs.forEach(function (acc) {
+    acc.userName = acc.owner
+      .toLocaleLowerCase()
+      .split(' ')
+      .map(name => name.at(0))
+      .join('');
+    console.log(acc.userName);
+  });
+}
+
+createUsernames(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -195,3 +236,78 @@ function checkDogs(dogsJulia, dogsKate) {
 const JuliaData1 = [3, 5, 2, 12, 7];
 const KateData1 = [4, 1, 15, 8, 3];
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+
+const eurToUsd = 1.1;
+
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+const movementsUSDfor = [];
+
+const movementsDescriptions = movements.map(
+  (mov, i, arr) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+
+// filter() : 조건에 맞는 값들을 반환함
+const deposits = movements.filter(mov => mov > 0);
+
+const depositsFor = [];
+for (const mov of deposits) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+
+const withdrawalsFor = [];
+for (const mov of withdrawals) withdrawalsFor.push(mov);
+console.log(withdrawalsFor);
+
+// reduce() : 배열길이만큼 실행
+// acc 매개변수 : reduce가 끝나기전까지 retrun 값 + acc를 함, 초기값은 아래 0 처럼 지정하지 않으면 배열[0] 값을 가져감
+// cur 매개변수 : 현재 배열값
+const balance = movements.reduce(function (acc, cur, i, arr) {
+  console.log(`acc: ${acc}, cur: ${cur}`);
+  return acc + cur;
+}, 0);
+
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max);
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+function calcAverageHumanAge(ages) {
+  const humanAges = ages.map(age => (age > 2 ? 16 + age * 4 : 2 * age));
+  const adults = humanAges.filter(age => age >= 18);
+
+  // 평균을 가져오는 방법 두가지
+  //const average = adults.reduce((acc, cur, i, arr) => acc + cur, 0) / adults.length;
+  const average = adults.reduce(
+    (acc, cur, i, arr) => acc + cur / arr.length,
+    0
+  );
+  console.log(ages);
+  console.log(average);
+}
+
+calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
