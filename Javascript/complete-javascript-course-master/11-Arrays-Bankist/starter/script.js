ce75f7cd6a063f9c0bf6e665c1946971e91b2501
ryongmi <62281754+ryongmi@users.noreply.github.com>
2022-10-24 22:04:36 +0900

    Challenge #3
    기존 함수 화살표 함수 및 체인으로 변경

--- Javascript/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ Javascript/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -311,3 +311,26 @@ function calcAverageHumanAge(ages) {
 
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const calcAverageHumanAgeArow = ages => {
+  const average = ages
+    .map(age => (age > 2 ? 16 + age * 4 : 2 * age))
+    .filter(age => age >= 18)
+    .reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+  console.log(average);
+};
+
+calcAverageHumanAgeArow([5, 2, 4, 1, 15, 8, 3]);
+calcAverageHumanAgeArow([16, 6, 10, 5, 6, 1, 4]);
