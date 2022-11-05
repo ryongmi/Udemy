e24e432497edaf07ad110819abc1444249f479f8
ryongmi <62281754+ryongmi@users.noreply.github.com>
2022-11-05 23:28:47 +0900

    파일 시스템 리펙토링

--- Node.js/The Complete Guide/Section_07/models/product.js
+++ Node.js/The Complete Guide/Section_07/models/product.js
@@ -1,6 +1,19 @@
 const fs = require("fs");
 const path = require("path");
 
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) cb([]);
+    else cb(JSON.parse(fileContent));
+  });
+};
+
 module.exports = class Product {
   // constructor : 다른 언어에서 사용하는 생성자와 같은 역홀
   // 한 클래스안에 하나만 정의 할 수 있음
@@ -10,33 +23,15 @@ module.exports = class Product {
   }
 
   save() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile((products) => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
+      fs.writeFile(p, JSON.stringify(products), (arr) => {
+        console.log(arr);
       });
     });
   }
 
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      } else cb(JSON.parse(fileContent));
-    });
+    getProductsFromFile(cb);
   }
 };
