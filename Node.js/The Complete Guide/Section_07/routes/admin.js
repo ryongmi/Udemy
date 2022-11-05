46584d6ddd0e485cb8bd8638a771b1e5992c0fd5
ryongmi <62281754+ryongmi@users.noreply.github.com>
2022-11-05 21:13:29 +0900

    Controller 작업
    MVC 모델중 Controller 파일 생성 및 로직 구현
    라우터 함수를 담아 놓아, 경로에 진입하면 해당 함수를 실행하게 하는 역활을 하는걸로 보임

--- Node.js/The Complete Guide/Section_07/routes/admin.js
+++ Node.js/The Complete Guide/Section_07/routes/admin.js
@@ -1,45 +1,12 @@
-const path = require("path");
-
 const express = require("express");
 
-const rootDir = require("../util/path");
-
-// 라우터 생성
 const router = express.Router();
 
-// 배열에 req.body값을 담아 사용할 경우
-// 배열에 담은 데이터가 서버가 실행되는 돋안 사라지지않고, 다른 유저에게도 같은 데이털르 보여주는 단점이있음
-const products = [];
+const productsController = require("../controllers/products");
 
-// use('/add-product', (req, res, next) : use(경로, (데이터 매개변수)) : 경로를 설정하여 실행될 미들웨어를 선택할 수 있음
-// 위에서 아래로 읽기 때문에 url에 /add-product이 붙어있으면 add-product 미들웨어가 실행된다
-router.get("/add-product", (req, res, next) => {
-  // send() : 응답값
-  // res.send(
-  //   "<form action='/admin/add-product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></input></form>"
-  // );
-  //res.sendFile(path.join(rootDir, "views", "add-product.html"));
+// Express 라우터에 getAddProduct 함수를 저장하는 구문
+// get - /add-product 의 요청이 들어올때 함수가 실행되게끔 설정
+router.get("/add-product", productsController.getAddProduct);
+router.post("/add-product", productsController.postAddProduct);
 
-  // path : 중복내용을 하나의 파일로 만들었을때, 파일마다 다른 action값을 주기위해 사용
-  res.render("add-product", {
-    title: "Add Product",
-    path: "/admin/add-product",
-    fromsCSS: true,
-    productCSS: true,
-    activeShop: true,
-    activeProduct: true,
-  });
-});
-
-// post() : post 요청일때만 실행됨
-// get() : get 요청일떄만 실행됨
-// use() : post, get, 둘다 실행됨
-router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
-
-  // redirect() : 경로 재설정
-  res.redirect("/");
-});
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
--- Node.js/The Complete Guide/Section_07/routes/shop.js
+++ Node.js/The Complete Guide/Section_07/routes/shop.js
@@ -1,27 +1,9 @@
-// Node.js에서 지원, 코어 모듈
-const path = require("path");
-
 const express = require("express");
-
-const rootDir = require("../util/path");
-const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  const products = adminData.products;
-  // 템플릿의 기본 폴더를 views로 잡아놔서 경로는 필요없음
-  // pug 를 템플릿으로 사용하겠다고 명시하여 확장자는 필요없음
-  // shop 파일을 보내며, 두번째 매개변수는 pug파일, 즉 html에서 사용할 변수값이다.
-  // 파일이름과 같이 보냄으로써 shop 파일에서 'prods', 'title' 두개의 값을 사용할 수 있게됨
-  res.render("shop", {
-    prods: products,
-    title: "Shop",
-    path: "/",
-    hasProduct: products.length > 0,
-    productCSS: true,
-    activeShop: true,
-  });
-});
+const productsController = require("../controllers/products");
+
+router.get("/", productsController.getProducts);
 
 module.exports = router;
