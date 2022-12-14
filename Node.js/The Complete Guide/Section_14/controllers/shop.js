f68bb0646361e50e324bedb51e06b66eddf9c0a8
ryongmi <62281754+ryongmi@users.noreply.github.com>
2022-12-14 22:48:35 +0900

    섹션 14 파일 세팅

---
+++ Node.js/The Complete Guide/Section_14/controllers/shop.js
@@ -0,0 +1,125 @@
+const Product = require("../models/product");
+const Order = require("../models/order");
+
+exports.getIndex = (req, res, next) => {
+  Product.find()
+    .then((products) => {
+      res.render("./shop/index", {
+        prods: products,
+        title: "Shop",
+        path: "/",
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.find()
+    .then((products) => {
+      console.log(products);
+      res.render("./shop/product-list", {
+        prods: products,
+        title: "All Products",
+        path: "/products",
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getProduct = (req, res, next) => {
+  // productId - shop.js에서 사용한 동적 url변수
+  // :productId - 이렇게 사용
+  const prodId = req.params.productId;
+
+  Product.findById(prodId)
+    .then((product) => {
+      res.render("./shop/product-Detail", {
+        product: product,
+        title: product.title,
+        path: "/products",
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getCart = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items;
+      res.render("./shop/cart", {
+        title: "Your Cart",
+        path: "/cart",
+        products: products,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId)
+    .then((product) => {
+      return req.user.addToCart(product);
+    })
+    .then((result) => {
+      console.log(result);
+      res.redirect("./cart");
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+
+  req.user
+    .deleteItemFromCart(prodId)
+    .then((result) => res.redirect("/cart"))
+    .catch((err) => console.log(err));
+};
+
+exports.postOrder = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user,
+        },
+        products: products,
+      });
+
+      return order.save();
+    })
+    .then((result) => {
+      return req.user.clearCart();
+    })
+    .then(() => {
+      res.redirect("./orders");
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getOrders = (req, res, next) => {
+  Order.find({ "user.userId": req.user._id })
+    .then((orders) => {
+      console.log(orders);
+      res.render("./shop/orders", {
+        title: "Orders",
+        path: "/orders",
+        orders: orders,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+// exports.getCheckout = (req, res, next) => {
+//   res.render("./shop/checkout", {
+//     title: "Checkout",
+//     path: "/checkout",
+//   });
+// };
