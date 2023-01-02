ac41ba2e4412ab44364f521db887645df4b009d8
ryongmi <62281754+ryongmi@users.noreply.github.com>
2023-01-02 23:27:05 +0900

    섹션 15 파일 세팅

---
+++ Node.js/The Complete Guide/Section_15/app.js
@@ -0,0 +1,93 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+
+const MONGODB_URI =
+  "mongodb+srv://User:231612@cluster0.10vyadb.mongodb.net/shop";
+
+// express 함수 및 로직을 받아옴
+const app = express();
+
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
+
+const errorController = require("./controllers/error");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
+const User = require("./models/user");
+
+// const User = require("./models/user");
+
+// pug에서 image - src 같이 문자열로 넣어야 하는곳은 src="#{변수명}"으로 넣는것이 아니라 src=변수명 으로 넣으면 작동함
+
+// 'pug' 라는 템플릿 엔진을 사용하겠다고 명시
+// 내장 엔진이여서 set으로 함
+app.set("view engine", "pug");
+
+// 템플릿 엔진의 기본 폴더를 설정
+// views 폴더가 기본 폴더로 설정됨
+app.set("views", "views");
+
+// bodyParser.urlencoded() : req.body 값을 분석해서 알려줌
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// 정적으로 파일의 경로를 지정
+// 정적의미 : 다른 미들웨어를 거쳐서 처리되지 않고, 바로 파일 시스템에 포워딩됨
+app.use(express.static(path.join(__dirname, "public")));
+
+// 세션 설정
+app.use(
+  session({
+    secret: "my session",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+// 라우터안의 경로가 /admin으로 공통될때, 라우터 파일이 아닌, 호출할때 경로를 지정할 수 있음
+// 이렇게 지정하게되면, /admin으로 경로가 시작되는 것들만 실행되게 할 수 있음
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "TurTle",
+          email: "turtle@test.com",
+          cart: { items: [] },
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ Node.js/The Complete Guide/Section_15/controllers/admin.js
@@ -0,0 +1,117 @@
+const Product = require("../models/product");
+
+exports.getAddProduct = (req, res, next) => {
+  res.render("./admin/edit-product", {
+    title: "Add Product",
+    path: "/admin/add-product",
+    editing: false,
+    isAuthenticated: req.session.isLoggedIn,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const description = req.body.description;
+  const price = req.body.price;
+  const product = new Product({
+    title: title,
+    price: price,
+    description: description,
+    imageUrl: imageUrl,
+    // Mongoose에서는 id를 정확히 지정하지 않고, user를 넘겨도, db에서 자동으로 id를 찾아 넣어줌
+    userId: req.user,
+    //userId: req.user._id,
+  });
+
+  product
+    .save()
+    .then((result) => {
+      console.log("Created Product");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.find()
+    // - 를 사용하면 그 데이터는 보여주지 않음.
+    //.select("title price -_id")
+    // 왼쪽에 있는것을 참조하여 다양한 데이터를 가져옴.
+    // 오른쪽에 있는 데이터만 보여줌
+    // _id는 명시적으로 빼지 않는 이상, 항상 가져옴
+    //.populate("userId", "name")
+    .then((products) => {
+      res.render("./admin/products", {
+        title: "Admin Products",
+        path: "/admin/products",
+        prods: products,
+        isAuthenticated: req.session.isLoggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getEditProduct = (req, res, next) => {
+  // req.query : 쿼리 매개변수로 url에서 주고 받고 할 수 있음
+  // url?edit=true -> edit은 키값이되며, true값을 지님
+  const editMode = req.query.edit === "true";
+
+  if (!editMode) {
+    return res.redirect("/");
+  }
+
+  const prodId = req.params.productId;
+
+  Product.findById(prodId)
+    .then((product) => {
+      if (!product) {
+        return res.redirect("/");
+      }
+
+      res.render("./admin/edit-product", {
+        title: "Edit Product",
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: product,
+        isAuthenticated: req.session.isLoggedIn,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updateedTitle = req.body.title;
+  const updateedPrice = req.body.price;
+  const updateedImageUrl = req.body.imageUrl;
+  const updateedDesc = req.body.description;
+
+  Product.findById(prodId)
+    .then((product) => {
+      product.title = updateedTitle;
+      product.price = updateedPrice;
+      product.imageUrl = updateedImageUrl;
+      product.description = updateedDesc;
+
+      return product.save();
+    })
+    .then((result) => {
+      console.log("Updated Product");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+
+  Product.findByIdAndRemove(prodId)
+    .then(() => {
+      console.log("Delete PRODUCT");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
+};
