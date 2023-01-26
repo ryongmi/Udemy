1b7b560b8908ac3053eab182eee044513911a0b5
ryongmi <62281754+ryongmi@users.noreply.github.com>
2023-01-26 23:50:47 +0900

    파일 업로드, 저장 및 삭제
    1. 클라이언트에 파일 전송하는 방법 2가지 - send(), 스트리밍
    2. 서버에 파일 저장하는 방법 - pdfkit이란 npm 사용
    3. 품목 이미지 수정 또는 품목 삭제 시  서버에 저장되어 있는 이미지 삭제 구현

--- Node.js/The Complete Guide/Section_20/controllers/admin.js
+++ Node.js/The Complete Guide/Section_20/controllers/admin.js
@@ -2,6 +2,7 @@
 const { validationResult } = require("express-validator");
 
 const Product = require("../models/product");
+const fileHelper = require("../util/file");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("./admin/edit-product", {
@@ -189,7 +190,10 @@ exports.postEditProduct = (req, res, next) => {
 
       product.title = updateedTitle;
       product.price = updateedPrice;
-      product.imageUrl = image?.path ?? product.imageUrl;
+      if (image) {
+        fileHelper.deleteFile(product.imageUrl);
+        product.imageUrl = image.path;
+      }
       product.description = updateedDesc;
 
       return product.save().then((result) => {
@@ -207,9 +211,14 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-
-  //Product.findByIdAndRemove(prodId)
-  Product.deleteOne({ _id: prodId, userId: req.user._id })
+  Product.findById(prodId)
+    .then((product) => {
+      if (!product) {
+        return next(new Error("Product not found."));
+      }
+      fileHelper.deleteFile(product.imageUrl);
+      return Product.deleteOne({ _id: prodId, userId: req.user._id });
+    })
     .then(() => {
       console.log("Delete PRODUCT");
       res.redirect("/admin/products");
--- Node.js/The Complete Guide/Section_20/controllers/shop.js
+++ Node.js/The Complete Guide/Section_20/controllers/shop.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const PDFDocument = require("pdfkit");
 
 const Product = require("../models/product");
 const Order = require("../models/order");
@@ -170,24 +171,65 @@ exports.getInvoice = (req, res, next) => {
 
       const invoiceName = "invoice-" + orderId + ".pdf";
       const invoicePath = path.join("data", "invoices", invoiceName);
-      fs.readFile(invoicePath, (err, data) => {
-        if (err) return next(err);
 
-        // 응답값이 pdf라는것을 브라우저에게 알림
-        res.setHeader("Content-Type", "application/pdf");
-        // attachment : 다운로드 형식
-        // filename : 다운로드할때, 파일이름을 지정할 수 있음
-        // res.setHeader(
-        //   "Content-Disposition",
-        //   `attachment; filename=${invoiceName}`
-        // );
-        //inline : 기본값이며, 브라우저에서 열리게됨
-        res.setHeader(
-          "Content-Disposition",
-          "inline; filename='" + invoiceName + "'"
-        );
-        res.send(data);
+      // PDFDocument : PDF를 생성할 수 있게해주는 npm
+      // PDF 파일을 서버에 저장하거나, 클라이언트에 전송할 수 있음
+      const pdfDoc = new PDFDocument();
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader(
+        "Content-Disposition",
+        "inline; filename='" + invoiceName + "'"
+      );
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+
+      pdfDoc.fontSize(26).text("Invoice", {
+        underline: true,
       });
+      pdfDoc.text("------------------------------");
+      let totalPrice = 0;
+      order.products.forEach((prod) => {
+        totalPrice += prod.quantity * prod.product.price;
+        pdfDoc
+          .fontSize(14)
+          .text(
+            `${prod.product.title} - ${prod.quantity} x $${prod.product.price}`
+          );
+      });
+      pdfDoc.text("--------------------------------------------------------");
+      pdfDoc.fontSize(20).text(`Total Price: $${totalPrice}`);
+      pdfDoc.end();
+
+      // * 이 방식은 서버에서 파일을 읽어들여 한번에 클라이언트로 보내주는 방법이며, 많은 요청이 발생하면 서버의 메모리 여유가 없음 *
+      // 작은 파일을 전송할때 권장됨
+      // fs.readFile(invoicePath, (err, data) => {
+      //   if (err) return next(err);
+
+      //   // 응답값이 pdf라는것을 브라우저에게 알림
+      //   res.setHeader("Content-Type", "application/pdf");
+      //   // attachment : 다운로드 형식
+      //   // filename : 다운로드할때, 파일이름을 지정할 수 있음
+      //   // res.setHeader(
+      //   //   "Content-Disposition",
+      //   //   `attachment; filename=${invoiceName}`
+      //   // );
+      //   //inline : 기본값이며, 브라우저에서 열리게됨
+      //   res.setHeader(
+      //     "Content-Disposition",
+      //     "inline; filename='" + invoiceName + "'"
+      //   );
+      //   res.send(data);
+      // });
+
+      // * 이 방식은 서버에서 파일을 클라이언트로 스트리밍하여 클라이언트에서 하나의 파일이 되게 함. 작게 분할하여 연속적으로 보내는 방식. 서버의 메모리 여유가 있음 *
+      // 큰 파일을 전송할때 권장됨
+      // const file = fs.createReadStream(invoicePath);
+      // res.setHeader("Content-Type", "application/pdf");
+      // res.setHeader(
+      //   "Content-Disposition",
+      //   "inline; filename='" + invoiceName + "'"
+      // );
+      // file.pipe(res);
     })
     .catch((err) => next(err));
 };
