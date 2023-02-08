80873d3a204c1b6e5897c069b40f671822eaf423
ryongmi <62281754+ryongmi@users.noreply.github.com>
2023-02-08 23:29:05 +0900

    CORS 오류
    클라이언트와 서버가 서로 다른 도메인에서 요청, 응답을 했을때 발생하는 오류로 방지하기 위해서는 헤더 설정을 해줘야함

--- Node.js/The Complete Guide/Section_24/app.js
+++ Node.js/The Complete Guide/Section_24/app.js
@@ -6,6 +6,15 @@ const feedRoutes = require("./routes/feed");
 //app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
+app.use((req, res, next) => {
+  // CORS 에러 방지하기 위한 구문
+  // 클라이언트와 서버의 도메인(IP, 포트)가 다를때, 요청이나 응답을 보낼때 발생함
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/feed", feedRoutes);
 
 app.listen(8080);
