bf9f8258ed3ca97c47b930cd8e6b22faefc8e7a2
ryongmi <62281754+ryongmi@users.noreply.github.com>
2023-02-13 23:43:19 +0900

    서로 다른 서버에서 API 통신
    클라이언트와 서버를 다른 도메인에서 실행하여, REST API를 작성해 데이터를 주고 받음
    
    프론트엔드에서는 코드가 다 보이기 때문에 공격자가 코드를 변경하여 유효성체크를 피할수도 있어, 백엔드에서도 유효성을 추가함

--- Node.js/The Complete Guide/Section_24/controllers/feed.js
+++ Node.js/The Complete Guide/Section_24/controllers/feed.js
@@ -1,15 +1,43 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: "First Post", contents: "This is the first post!" }],
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        contents: "This is the first post!",
+        imageUrl: "images/ori.jpg",
+        creator: {
+          name: "TurTle",
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorrect",
+      errors: errors.array(),
+    });
+  }
   const title = req.body.title;
   const contents = req.body.contents;
   // create post in db
   res.status(201).json({
     message: "Post created successfully!",
-    post: { id: new Date().toDateString(), title: title, contents: contents },
+    post: {
+      _id: new Date().toDateString(),
+      title: title,
+      contents: contents,
+      creator: {
+        name: "TurTle",
+      },
+      createdAt: new Date(),
+    },
   });
 };
