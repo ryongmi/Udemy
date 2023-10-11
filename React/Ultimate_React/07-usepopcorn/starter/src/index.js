c5dbdd546f16df72d596a8605d7db46795a6fbfc
ryongmi <62281754+ryongmi@users.noreply.github.com>
2023-10-11 16:57:07 +0900

    PropTypes, props default
    props에 기본값과 속성 유효성 검사 하는법

---
+++ React/Ultimate_React/07-usepopcorn/starter/src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+
+import StarRating from "./StarRating";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      className="test"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={2} />
+
+    <Test /> */}
+  </React.StrictMode>
+);
