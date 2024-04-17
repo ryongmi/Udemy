63c2092020147115a92412231f5c8fd8cfee2396
ryongmi <62281754+ryongmi@users.noreply.github.com>
2024-04-17 13:47:26 +0900

    interceptor 사용
    router 진입전이나 작업후에 추가 로직을 넣을 수 있고, dto를 정의하여 사용자에게 보내고 싶은 속성만 보낼 수 있게 해줌

---
+++ NestJS/mycv/src/users/user.entity.ts
@@ -0,0 +1,36 @@
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+// import { Exclude } from 'class-transformer';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  // @Exclude() // 서버에서 클라이언트에 JSON데이터를 보낼때, password는 제외하는 데코레이션
+  password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User with id', this.id);
+  }
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed User with id', this.id);
+  }
+}
---
+++ NestJS/mycv/src/users/users.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    // 엔티티 인스턴스를 만들고 저장을 할 경우
+    // entity파일에서 로그 등 추가 로직을 설정할 수 있음
+    // 이렇게 하지 않고 save에 바로 저장할 경우, 이런 이점을 살리지 못해 인스턴스를 먼저 만드는게 좋음
+    const user = this.repo.create({ email, password });
+
+    return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    return this.repo.remove(user);
+  }
+}
