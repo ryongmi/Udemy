1ab05a5a23d492bf0acd7a432b91b5b63a3b8a0a
ryongmi <62281754+ryongmi@users.noreply.github.com>
2024-04-18 17:22:40 +0900

    Guard, Param 데코레이터 사용
    Param 데코레이터를 사용할때, 인터셉터만으로 사용이 가능하긴함
    
    데코레이터만 사용하는 것은 의존성 문제로 serivce 사용이 불가능하기에, 인터셉터에서 service를 사용하며 쿼리를 활용함

--- NestJS/mycv/src/users/users.controller.ts
+++ NestJS/mycv/src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Query,
   NotFoundException,
   Session,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -16,6 +17,9 @@ import { UsersService } from './users.service';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -25,9 +29,15 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
+  // @Get('whoami')
+  // whoAmI(@Session() session: any) {
+  //   return this.usersService.findOne(session.userId);
+  // }
+
   @Get('whoami')
-  whoAmI(@Session() session: any) {
-    return this.usersService.findOne(session.userId);
+  @UseGuards(AuthGuard)
+  whoAmI(@CurrentUser() user: User) {
+    return user;
   }
 
   @Post('/signout')
