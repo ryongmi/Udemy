ce19690d9745d36d747f1922a6181289fc6ba828
ryongmi <62281754+ryongmi@users.noreply.github.com>
2024-05-06 07:07:25 +0900

    redis pipeline
    redis에서 여러 명령어를 한번에 실행하기 위해 pipeline을 사용함
    
    사용이유는 여러 명령어를 한번씩 실행하면 redis에 여러번 요청을 하게되어 시간이 걸리며, 클라우드를 사용할 경우 비용이 증가하는 문제가 있음.
    
    이를 해결하기 위해 명령어를 한번에 묶어 하나의 요청으로 결과를 받아옴

--- Redis/rbay/sandbox/index.ts
+++ Redis/rbay/sandbox/index.ts
@@ -2,17 +2,24 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const run = async () => {
-	await client.hSet('car', {
+	await client.hSet('car1', {
 		color: 'red',
 		year: 1950
 	});
-	const car = await client.hGetAll('car');
+	await client.hSet('car2', {
+		color: 'green',
+		year: 1955
+	});
+	await client.hSet('car3', {
+		color: 'blue',
+		year: 1960
+	});
 
-	if (Object.keys(car).length === 0) {
-		console.log('Car not found, respond with 404');
-		return;
-	}
-
-	console.log(car);
+	const result = await Promise.all([
+		client.hGetAll('car1'),
+		client.hGetAll('car2'),
+		client.hGetAll('car3')
+	]);
+	console.log(result);
 };
 run();
--- Redis/rbay/src/services/queries/items/items.ts
+++ Redis/rbay/src/services/queries/items/items.ts
@@ -1,7 +1,40 @@
+import { itemsKey } from '$services/keys';
+import { client } from '$services/redis';
 import type { CreateItemAttrs } from '$services/types';
+import { genId } from '$services/utils';
+import { deserialize } from './deserialize';
+import { serialize } from './serialize';
 
-export const getItem = async (id: string) => {};
+export const getItem = async (id: string) => {
+	const item = await client.hGetAll(itemsKey(id));
 
-export const getItems = async (ids: string[]) => {};
+	if (Object.keys(item).length === 0) {
+		return null;
+	}
 
-export const createItem = async (attrs: CreateItemAttrs, userId: string) => {};
+	return deserialize(id, item);
+};
+
+export const getItems = async (ids: string[]) => {
+	const commands = ids.map((id) => {
+		return client.hGetAll(itemsKey(id));
+	});
+
+	const results = await Promise.all(commands);
+
+	return results.map((result, index) => {
+		if (Object.keys(result).length === 0) {
+			return null;
+		}
+
+		return deserialize(ids[index], result);
+	});
+};
+
+export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
+	const id = genId();
+
+	await client.hSet(itemsKey(id), serialize(attrs));
+
+	return id;
+};
