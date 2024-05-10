6b7b1aadcfac9ab110ea535ea085a3a7ac513f5a
ryongmi <62281754+ryongmi@users.noreply.github.com>
2024-05-10 16:08:52 +0900

    hyperLogLog, List
    HyperLogLog는 실제로 데이터를 저장하지 않고, 내부의 알고리즘에서 값을 해시값으로 변환하여 기억만 하고 있음. 대략 12kb의 공간을 차지하여, 공간 절약에 유리함
    
    List는 양뱡향 리스트를 사용하며 값을 가지고 있고, 해당 값을 찾기 위해선 해당 인덱스를 알아야함

--- Redis/rbay/src/services/queries/bids.ts
+++ Redis/rbay/src/services/queries/bids.ts
@@ -1,7 +1,51 @@
+import { bidHistoryKey, itemsKey } from '$services/keys';
+import { client } from '$services/redis';
 import type { CreateBidAttrs, Bid } from '$services/types';
+import { DateTime } from 'luxon';
+import { getItem } from './items';
 
-export const createBid = async (attrs: CreateBidAttrs) => {};
+export const createBid = async (attrs: CreateBidAttrs) => {
+	const item = await getItem(attrs.itemId);
+	if (!item) {
+		throw new Error('Item does not exist');
+	}
+	if (item.price >= attrs.amount) {
+		throw new Error('Bid too low');
+	}
+	if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
+		throw new Error('Item closed to bidding');
+	}
+
+	const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
+
+	return Promise.all([
+		client.rPush(bidHistoryKey(attrs.itemId), serialized),
+		client.hSet(itemsKey(item.id), {
+			bids: item.bids + 1,
+			price: attrs.amount,
+			highestBidUserId: attrs.userId
+		})
+	]);
+};
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
-	return [];
+	const startIndex = -1 * offset - count;
+	const endIndex = -1 - offset;
+
+	const range = await client.lRange(bidHistoryKey(itemId), startIndex, endIndex);
+
+	return range.map((bid) => deserializeHistory(bid));
+};
+
+const serializeHistory = (amount: number, createdAt: number) => {
+	return `${amount};${createdAt}`;
+};
+
+const deserializeHistory = (stored: string) => {
+	const [amount, createdAt] = stored.split(';');
+
+	return {
+		amount: parseFloat(amount),
+		createdAt: DateTime.fromMillis(parseInt(createdAt))
+	};
 };
